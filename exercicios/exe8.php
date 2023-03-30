cf7092d4b05200a7bf4370f4f4986ac389405450
Necker08 <123278333+Necker08@users.noreply.github.com>
2023-03-30 13:01:13 -0300

    Add files via upload

---
+++ exercicios/exe8.php
@@ -0,0 +1,13 @@
+<?php
+
+$Produto = $_POST['num1']."<br>";
+$soma;
+$divisao;
+
+$soma =  $Produto * 0.15;
+
+$divisao = $produto / 5;
+
+echo " O valor total do produto é:  " . $soma;
+echo " O valor parcelado do produto é:  " . $soma." em 5x ";
+?>
---
+++ exercicios/exe8_recebe.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio</title>
+</head>
+<body>
+    <form method="POST" action ="exe7.php">
+    <label>Valor do Produto: <input type="nun1" name = "num1"/> </label><br>
+    <input type="submit" name="btnCalcular" value="Calcular">
+</form>
+</body>
+</html>
