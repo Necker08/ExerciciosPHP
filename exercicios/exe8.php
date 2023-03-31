ed59f52c383ad2c11176c2c837c856da54291d64
Necker08 <123278333+Necker08@users.noreply.github.com>
2023-03-31 14:20:51 -0300

    Add files via upload

--- exercicios/exe8.php
+++ exercicios/exe8.php
@@ -1,13 +1,24 @@
-<?php
-
-$Produto = $_POST['num1']."<br>";
-$soma;
-$divisao;
-
-$soma =  $Produto * 0.15;
-
-$divisao = $produto / 5;
-
-echo " O valor total do produto é:  " . $soma;
-echo " O valor parcelado do produto é:  " . $soma." em 5x ";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculadora de Parcelamento</title>
+</head>
+<body>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<label for="valor">Valor do Produto:</label>
+		<input type="text" name="valor" id="valor" required><br><br>
+		<input type="submit" name="submit" value="Calcular">
+	</form>
+	<br>
+	<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$valor = $_POST["valor"];
+		$valor_com_juros = $valor * 1.15;
+		$valor_parcela = $valor_com_juros / 5;
+		$total_compra = $valor_com_juros;
+		echo "Valor da Parcela: R$ " . number_format($valor_parcela, 2, ",", ".");
+		echo "<br>Total da Compra: R$ " . number_format($total_compra, 2, ",", ".");
+	}
+	?>
+</body>
+</html>
